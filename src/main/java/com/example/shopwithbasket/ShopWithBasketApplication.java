3c0540a8fbb379484b215d453ddb4c2709ca83ca
Maks <max@moais.ru>
2023-03-23 10:56:38 +0300

    Initial commit

---
+++ src/main/java/com/example/shopwithbasket/ShopWithBasketApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopwithbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopWithBasketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopWithBasketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shopwithbasket/ShopWithBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopwithbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopWithBasketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
